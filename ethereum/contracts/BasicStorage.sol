f36711b74a0557cea9d707efaa8e68c086cd1e64
Anwar <aalruwai@stevens.edu>
2021-01-18 18:31:21 -0500

    Update BasicStorage.sol

--- ethereum/contracts/BasicStorage.sol
+++ ethereum/contracts/BasicStorage.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.4.16 <0.9.0;
 
 contract BasicStorage {
@@ -5,12 +6,12 @@ contract BasicStorage {
     string message;
     uint store;
     
-    function set(uint _store, string _message) public {
+    function set(uint _store,  string memory _message) public {
         store = _store;
         message = _message;
     }
     
-    function getMessage() public view returns( string){
+    function getMessage() public view returns( string memory){
         return message;
     }
     
