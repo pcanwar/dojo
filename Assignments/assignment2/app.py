4747827b184a1057eb481831dffca0af486bbf44
Anwar <aalruwai@stevens.edu>
2021-02-11 04:21:02 -0500

    Update app.py

--- Assignments/assignment2/app.py
+++ Assignments/assignment2/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+# from flask_cors import CORS
 from blockchain import Blockchain
 from argparse import ArgumentParser
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 
 @app.route('/', methods=['GET'])
@@ -18,12 +18,18 @@ def chian():
 
 @app.route('/mine', methods=['POST'])
 def mine():
+    """ mine a block """
+    pass
 
 @app.route('/opentxs', methods=['GET'])
 def opentxs():
+    """ get the unconfirmed transactions or any transaction has not been included in a block """
+    pass
 
 @app.route('/sendtx', methods=['POST'])
 def sendtx():
+    """ send a transaction"""
+    pass
 
 
 if __name__ == '__main__':
