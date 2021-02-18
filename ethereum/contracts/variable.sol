d0627aef2729bb87589082871d13fa92460ff4b3
Anwar <aalruwai@stevens.edu>
2021-02-18 12:27:38 -0500

    Create variable.sol

---
+++ ethereum/contracts/variable.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.5.1 <0.8.0;
+
+contract Variable {
+   // boolean 
+   bool public sold;
+   
+   // integers
+   uint256 public numb1 = 10;
+   int8 public x = 127;
+    //int8 public x = 129; it shows error cuz max 127
+   
+}
