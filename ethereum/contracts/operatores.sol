b4a222ced39f9003e18294eb88b7f8481a953fdf
Anwar <aalruwai@stevens.edu>
2021-02-18 23:38:59 -0500

    Create operatores.sol

---
+++ ethereum/contracts/operatores.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.5.0 <0.8.0;
+
+contract Operatores {
+    // https://docs.soliditylang.org/en/v0.8.1/types.html
+    
+    // Arithmetic operators: +, -, *, /, % (modulo), ** (exponentiation)
+    uint a = 3 ** 2;
+    
+    
+    //logical operators: comparisons: <=, <, ==, !=, >=, > (evaluate to bool)
+    bool hashMoney = !false;
+    
+    //Bit operators: &, |, ^ (bitwise exclusive or), ~ (bitwise negation)
+    // byte contractValue = 0x02 ;
+    
+    //Shift operators: << (left shift), >> (right shift)
+
+
+}
