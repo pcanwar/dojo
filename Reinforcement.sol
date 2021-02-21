9c77c9a55bd1ca80247c3cc64b377bc605b05301
Anwar <aalruwai@stevens.edu>
2021-02-21 00:08:10 -0500

    Create Reinforcement.sol

---
+++ Reinforcement.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * @title IsMultiple
+ * @ Question 1) This is a short solidity function, isMultiple(n, m), 
+ * that takes two integer values and returns True if n is a multiple of m, 
+ * and False otherwise.
+ * 
+ * @ Question 2) A short solidity function takes two integer values
+ * and returns the maximum element between two integers
+ * 
+ */
+
+
+pragma solidity >=0.6.0 <0.8.0;
+
+contract Math{
+
+    function isMultiple(uint8 _n, uint8 _m) public pure returns(bool) {
+        
+        // Write your code here
+    }
+    
+    function max(uint256 _n, uint256 _m) public pure returns(uint256) {
+                // Write your code here
+
+           
+       }
+
+}
