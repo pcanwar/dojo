e0f38730c757652c316cd25e2de16b1ef3619180
Anwar <aalruwai@stevens.edu>
2021-03-01 03:47:47 -0500

    Create readBlock_readBalance.js

---
+++ ethereum/web3/readBlock_readBalance.js
@@ -0,0 +1,21 @@
+var Web3 = require('web3')
+
+var url = "https://mainnet.infura.io/v3/ID"
+var web3 = new Web3(url);
+
+myaddress = '0xa'
+
+web3.eth.getBalance(myaddress, function(error, result) {
+    var balance = result
+    var ethBalance = web3.utils.fromWei(balance, 'ether');
+    console.log( `The amount in eth: ${ethBalance}`);
+
+});
+
+blockHashOrNum = 100
+
+web3.eth.getBlock("latest", function(error, result){
+
+        console.log(result)
+
+})
