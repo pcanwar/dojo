9162c79e4cbbd8467681bd3f794231c99d806602
Anwar <aalruwai@stevens.edu>
2021-03-17 09:38:22 -0400

    updated to 0.8

--- ethereum/security/safeWithdraw.sol
+++ ethereum/security/safeWithdraw.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT 
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+// `SafeMath` is no longer needed starting with Solidity 0.8. The compiler now has built in overflow checking.
+
 contract Fundraising {
+    //
     using SafeMath for uint256;
     mapping(address => uint) public balances;
     
@@ -14,23 +17,28 @@ contract Fundraising {
         _;
         getItLocked = false;
     }
+    
+        
+    modifier noCall{
+        require(msg.sender == tx.origin, "no contract is allowed");
+        _;
+    }
 
     
     function deposit() public payable {
         balances[msg.sender] += msg.value;
     }
 
-    function withdraw() singleEntry public {
-        // checkpoints
-        require(msg.sender == tx.origin, "no contract is allowed");
+    function withdraw() singleEntry noCall public {
+        // 1st checkpoints
         require(balances[msg.sender] >= 0);
-        // changepoints
+        
+        // 2nd changepoints
         uint amountToWithdraw = balances[msg.sender];
         balances[msg.sender] = balances[msg.sender].sub(amountToWithdraw);
-        // (bool success, ) = msg.sender.call{value: amountToWithdraw}("");
-        // require(success,"Unsucced");
-        // interact points
-        msg.sender.transfer(amountToWithdraw);
+     
+        // 3rd interact points
+        payable(msg.sender).transfer(amountToWithdraw);
         
     }
 
